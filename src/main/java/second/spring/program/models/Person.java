fe8b31c289811a46e547e7eb02fa2c4f1bf96e23
Alexey Sitiy <alexeysitiy@gmail.com>
2022-07-07 11:56:05 +0300

    done

--- src/main/java/second/spring/program/models/Person.java
+++ src/main/java/second/spring/program/models/Person.java
@@ -18,15 +18,28 @@ public class Person {
     @Email(message = "Email should be valid")
     private String email;
 
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Pattern(regexp = "[A-Z]\\w+, [A-Z]\\w+, \\d{5}",message = "You should enter a correct location as an example \"Ukraine, Kharkiv, 61000\"")
+    @NotEmpty(message = "Address should not be empty")
+    private String address;
+
     public Person() {
 
     }
 
-    public Person(int id, String name, int age, String email) {
+    public Person(int id, String name, int age, String email, String address) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.email = email;
+        this.address = address;
     }
 
     public int getId() {
